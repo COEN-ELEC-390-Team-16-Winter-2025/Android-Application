452dba39fbc707d46ac70d7d6648dc5075b10475
mikdriver77 <mi_drive@live.concordia.ca>
2025-02-26 17:59:04 -0500

    LandingPage created and weight/height page created

---
+++ app/src/main/java/com/drinkwise/app/LandingActivity.java
@@ -0,0 +1,35 @@
+package com.drinkwise.app;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LandingActivity extends AppCompatActivity {
+    private Button getStartedButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+        getStartedButton = findViewById(R.id.getStartedButton);
+
+        getStartedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to User Profile Page 1
+                Intent intent = new Intent(LandingActivity.this, UserProfileActivity1.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
