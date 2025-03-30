1c7cc6690a7786a1fa50ad353352148f94d5e7d5
Liyan Al-mosaria <liyana@softsim.ca>
2025-03-30 13:45:09 -0400

    info page added, need to fix the info button in the settings page.

---
+++ app/src/main/java/com/drinkwise/app/InfoActivity.java
@@ -0,0 +1,34 @@
+package com.drinkwise.app;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.drinkwise.app.R;
+
+public class InfoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        // Set up action bar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("Information Page");
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
