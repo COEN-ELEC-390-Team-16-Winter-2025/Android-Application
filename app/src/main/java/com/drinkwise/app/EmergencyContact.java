28fab73570d755e1cc27c570a86e4860f6071ec9
kisuk8 <axil-pacha@hotmail.com>
2025-03-29 22:27:49 -0400

    Added recycler view for emergency contacts

---
+++ app/src/main/java/com/drinkwise/app/EmergencyContact.java
@@ -0,0 +1,34 @@
+package com.drinkwise.app;
+
+import com.google.firebase.firestore.SnapshotMetadata;
+
+public class EmergencyContact {
+
+    private String name;
+    private String phone_no;
+    private String email;
+    private String relationship;
+
+    public EmergencyContact(String name, String phone_no, String email, String relationship) {
+        this.name = name;
+        this.phone_no = phone_no;
+        this.email = email;
+        this.relationship = relationship;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone_no() {
+        return phone_no;
+    }
+
+    public String getRelationship() {
+        return relationship;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
