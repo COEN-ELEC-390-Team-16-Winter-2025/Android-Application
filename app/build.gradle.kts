caf02f0b34427ac928535303bc3aff21acef204c
kisuk8 <axil-pacha@hotmail.com>
2025-04-02 13:08:10 -0400

    Current progress for task 33.3 - 33.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,11 @@ dependencies {
 
     implementation ("com.google.android.material:material:1.10.0")
 
+    //dependencies needed for google map
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("com.android.volley:volley:1.2.1")
+
 
     implementation(libs.appcompat)
     //implementation(libs.material)
